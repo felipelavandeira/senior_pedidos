2e495b92e97d0c926de28be9a8785f429711bca1
Felipe Lavandeira <felipe.lavandeira@gmail.com>
2021-04-12 13:26:06 -0300

    Iniciando o projeto

---
+++ src/main/java/com/lavandeira/senior_pedidos/SeniorPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.lavandeira.senior_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorPedidosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lavandeira/senior_pedidos/SeniorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavandeira.senior_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
